1a62710acadb3d12fc509cc9864aef628917c3ae
mkarnam <mkarnam@MH23.corp.miraclesoft.com>
2019-04-26 17:00:34 +0530

    new message committed

---
+++ NewRepTest/src/ManiMiracle.java
@@ -0,0 +1,15 @@
+
+public class ManiMiracle {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=a+b+c;
+		
+		System.out.println("out put"+d);
+	
+	}
+
+}
